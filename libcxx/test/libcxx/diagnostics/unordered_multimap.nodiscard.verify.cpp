d290444b6aa886b5e01ea3179e9e421f5f30cd83
Hristo Hristov <hghristov.rmm@gmail.com>
2025-12-03 07:45:07 +0200

    [libc++][unordered_map] Applied `[[nodiscard]]`
    [[nodiscard]] should be applied to functions where discarding the return value is most likely a correctness issue.
    
    - https://libcxx.llvm.org/CodingGuidelines.html
    - https://wg21.link/unord.multimap

---
+++ libcxx/test/libcxx/diagnostics/unordered_multimap.nodiscard.verify.cpp
@@ -0,0 +1,19 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03
+
+// Check that functions are marked [[nodiscard]]
+
+#include <unordered_map>
+
+void test() {
+  std::unordered_multimap<int, int> um;
+
+  um.empty(); // expected-warning {{ignoring return value of function declared with 'nodiscard' attribute}}
+}
